bed0b5958e31c86071fc2abd290029a2bd64d611
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-07-29 22:39:16 +0900

    アプリ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  devise_for :users
+  root 'top#index'
+
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, except: %i(index)
+  end
 end
